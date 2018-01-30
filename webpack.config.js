b3b80d88edf40c0aa289089d347cf54bd3780583
Dazhi Jiao <djiao@jhu.edu>
2018-01-29 23:21:35 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+        {
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        },
+        {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }
+    ]
+  },
+  devServer: {
+      contentBase: path.resolve(__dirname, "dist"),
+      historyApiFallback: true // To enable direct access to route
+  },
+  devtool: 'source-map',
+};
