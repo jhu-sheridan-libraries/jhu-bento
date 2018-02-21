d1985056aaeb65daaff93414e7d7e3c975c9676e
Dazhi Jiao <djiao@jhu.edu>
2018-02-21 15:43:31 -0500

    set up layout and styles

--- src/components/Bento.js
+++ src/components/Bento.js
@@ -68,38 +68,26 @@ class Bento extends Component {
 
   render() {
     return (
-      <Grid fluid>
-        <Row top="xs">
-          <Col xs={12} md={12} lg={12}>
-            <input ref="search" type="search" placeholder="Search" onKeyPress={ this.handleSearchBoxKeyPress } value={ this.state.searchTerm } onChange={ this.handleChange } />
-            <button className="btn btn-primary" onClick={ this.handleClick }>Fetch</button>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} md={6} lg={6} style={{ backgroundColor: "#ccff66" }}>
-            <CatalystContainer />
-          </Col>
-          <Col xs={12} md={6} lg={6} style={{ backgroundColor: "#ff9999" }}>
-            <LaraContainer />
-          </Col>          
-        </Row>
-        <Row>
-          <Col xs={12} md={6} lg={6} style={{ backgroundColor: "#66ccff" }}>
-            <ArchivesSpaceContainer />
-          </Col>
-          <Col xs={12} md={6} lg={6} style={{ backgroundColor: "#ff99ff" }}>
-            <EdsContainer />
-          </Col>          
-        </Row>
-        <Row>
-          <Col xs={12} md={6} lg={6} style={{ backgroundColor: "#cc99ff" }}>
-            <ScopusContainer />
-          </Col>
-          <Col xs={12} md={6} lg={6} style={{ backgroundColor: "yellow" }}>
-            <LibAnswersContainer />
-          </Col>          
-        </Row>
-      </Grid>
+      <div>
+        <div className="container search-container">
+          <div className="input-group">
+            <input type="text" name="q" id="q" className="q form-control" placeholder="Search" autoComplete="off" autoFocus="autofocus" value={ this.state.searchTerm } onKeyPress={ this.handleSearchBoxKeyPress } onChange={ this.handleChange }/>
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-primary search-btn" id="search" onClick={ this.handleClick }>
+                <span className="submit-search-text">Search</span>
+              </button>
+            </span>
+          </div>
+        </div>
+        <div className="main-container">
+        <CatalystContainer />
+        <LaraContainer />
+        <ArchivesSpaceContainer />
+        <EdsContainer />
+        <ScopusContainer />
+        <LibAnswersContainer />
+        </div>
+      </div>
     )
   }
 }
