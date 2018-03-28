8abe7b7ced301b19531d2d0296727b207578e582
Dazhi Jiao <djiao@jhu.edu>
2018-03-28 13:12:25 -0400

    Refactor widget to have cosistent structure for initial state and search results

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -7,26 +7,25 @@ class Widget extends Component {
   }
 
   render() {
-    if (this.props.numFound < 0) {  
-      return (
-        <div id={ this.props.id } className='bento-box'>
-          <h3>{ this.props.title }</h3>
-          <div>Please submit a search</div>
-        </div>
-      )
-    } else {
-      return (
-        <div id={ this.props.id } className='bento-box'>
-          <div className='bento-box-header' style={{ cursor: 'pointer' }}>
-            <h3><a href={ this.props.url }>{ this.props.title }</a></h3>
-            <span className='count'><a href={ this.props.url }>{ this.props.numFound.toLocaleString('en') }</a></span>
-          </div>
-          <div className='bento-content'>
-            { this.props.items.length ? this.props.items : 'No results found' }
-          </div>
-        </div>
-      )
+    let title = (<h3>{ this.props.title }</h3>)
+    let counter = ''
+    let content = 'Please submit a search'
+    if (this.props.numFound >= 0) {  
+      title = (<h3><a href={ this.props.url }>{ this.props.title }</a></h3>)
+      counter = (<span className='count'><a href={ this.props.url }>{ this.props.numFound.toLocaleString('en') }</a></span>)
+      content = this.props.items.length ? this.props.items : 'No results found'
     }
+    return (
+      <div id={ this.props.id } className='bento-box'>
+        <div className='bento-box-header'>
+          { title } 
+          { counter }
+        </div>
+        <div className='bento-content'>
+          { content }
+        </div>
+      </div>
+    )
   }
 }
 
