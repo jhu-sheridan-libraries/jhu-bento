a9734c35b1b08ec870918363a5fabe835645338d
Dazhi Jiao <djiao@jhu.edu>
2018-04-10 13:08:58 -0400

    Use rewire to test initial state in reducers

--- src/reducers/search.test.js
+++ src/reducers/search.test.js
@@ -1,12 +1,14 @@
 import faker from 'faker'
-import reducer from './search'
+import reducer, { __RewireAPI__ as RewiredApi } from './search'
 import * as Actions from '../actions' 
 import * as ActionTypes from '../actions/constants'
 
 describe('search reducer', () => {
   let value = faker.lorem.word()
 
-  it('should return the initial state'), () => {
-    expect(reducer(undefined, {}).toEqual({}))
-  }
+  it('should return the initial state', () => {
+    let initialState = RewiredApi.__get__('initialState')
+    expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
 })
